0302a242bf08af50db2fec486aefea95d3c79ed1
trompuditasoftwarein <trompuditasoftwarein@TOPICOS-INSTANC>
2020-09-19 22:27:06 +0000

    Se agrega proyecto para desacoplar el Main en una clase de Utilitarios

---
+++ Topicos/Topicos.Calentamiento/Job.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Topicos.Calentamiento
+{
+    public class Job
+    {
+        public void Do ()
+        {
+            Console.WriteLine("Hello World!");
+            var lasHerramientas = new Topicos.Utilitarios.Message();
+            var elMensaje = lasHerramientas.Say("Hello World!");
+            Console.WriteLine(elMensaje);
+        }
+    }
+}
