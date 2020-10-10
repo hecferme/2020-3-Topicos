263855d6f8f6fb2ce8c50347c858c6ad383ec5a8
HÉCTOR FERNÁNDEZ MÉNDEZ <FernandezMH@bccr.fi.cr>
2020-10-10 21:15:01 +0000

    Se agregan modelos de la bd NorthWind via EF Core Tools

---
+++ Topicos/Topicos.NorthWind.BaseDatos/MisModelos/Products.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Topicos.NorthWind.BaseDatos.Models
+{
+    public partial class Products
+    {
+        public short? UnidadesDesabastecidas { get 
+            { 
+                short? elResultado = 0;
+                if (UnitsInStock < ReorderLevel)
+                    elResultado = (short?)(UnitsInStock - ReorderLevel);
+                return elResultado;
+            }
+            set
+            { } 
+        }
+
+        public string SupplierName {
+            get 
+            {
+                string elResultado = string.Empty;
+                if (Supplier != null)
+                {
+                    elResultado = Supplier.CompanyName;
+                }
+                return elResultado;
+            } 
+            set
+            { }
+        }
+    }
+}
