92480ba950c79cb6792dc4c1d1c8d2e9081fb06b
HÉCTOR FERNÁNDEZ MÉNDEZ <FernandezMH@bccr.fi.cr>
2020-10-24 02:04:13 +0000

    Ejemplo API

---
+++ Topicos/ProductInfo.API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductInfo.API.Controllers
+{
+    [ApiController]
+    //[Route("api/[controller]")]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        //[HttpGet("api/products")]
+        [HttpGet]
+        public JsonResult GetProducts()
+        {
+            JsonResult resultado = new JsonResult (
+                new List<Object>()
+                {
+                    new {id = 10, Name = "Milk"},
+                    new {id = 20, Name = "Coffee"}
+                });
+            return resultado;
+        }
+    }
+}
