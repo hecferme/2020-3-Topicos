6adb738fd2f8b69eaa9994b15824859d5f35caee
HÉCTOR FERNÁNDEZ MÉNDEZ <FernandezMH@bccr.fi.cr>
2020-12-05 22:40:29 +0000

    Incorporación del API de productos para que invoque al BL

---
+++ Topicos/WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.MisModelos;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var elResultado = new List <ProductsDto> ();
+            return Ok(elResultado);
+        }
+
+        internal ProductsDto BuscarProducto(int orderId)
+        {
+            var elProducto = new ProductsDto ();
+            return elProducto;
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var elProducto = BuscarProducto(id);
+            if (elProducto == null)
+                return NotFound();
+            return Ok(elProducto);
+        }
+
+    }
+}
---
+++ Topicos/WebApplication1/MisModelos/ProductsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.MisModelos
+{
+    public class ProductsDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+        public bool Discontinued { get; set; }
+        public string CategoryName { get; set; }
+        public string SupplierName { get; set; }
+
+    }
+}
